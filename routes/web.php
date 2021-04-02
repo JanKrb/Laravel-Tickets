151005336ad3e79b3a9453a05abf817382f4983f
Jan Ruhfus <unknownditx@gmail.com>
2021-04-02 22:44:39 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -44,10 +44,17 @@ Route::group(['middleware' => 'auth'], function () {
     Route::put('/account/{accountid}', 'AccountController@changePasswordAccount')->name('changePasswordAccount'); // Update infos of account
     Route::delete('/account/{accountid}', 'AccountController@deleteAccount')->name('deleteAccount'); // Delete account
 
-    // Routes for Ticket System
+    // Routes for Ticket Views
     Route::get('/create-ticket', 'TicketController@createTicket')->name('createTicket'); // Show create ticket view
     Route::get('/tickets', 'TicketController@listTicket')->name('listTicket'); // List all tickets
     Route::get('/ticket-settings', 'TicketController@adminTicket')->name('adminTicket'); // Show ticket settings
     Route::get('/ticket/{ticketid}', 'TicketController@viewTicket')->name('showTicket'); // Show ticket
+
+    // Routes for Ticket Admin
+    Route::post('/ticket-settings/generic', 'Ticket\TicketAdminController@updateGenericSetting')->name('updateGenericSetting'); // Post Route for Ticket Admin / Generic Settings form
+    Route::post('/ticket-settings/status', 'Ticket\TicketAdminController@addNewStatus')->name('addNewStatus'); // Post Route for Ticket Admin / Tags form
+    Route::post('/ticket-settings/tag', 'Ticket\TicketAdminController@addNewTag')->name('addNewTag'); // Post Route for Ticket Admin / Tags form
+    Route::post('/ticket-settings/department', 'Ticket\TicketAdminController@addNewDepartment')->name('addNewDepartment'); // Post Route for Ticket Admin / Department form
+    Route::post('/ticket-settings/preset', 'Ticket\TicketAdminController@addNewPreset')->name('addNewPreset'); // Post Route for Ticket Admin / Preset form
 });
 
