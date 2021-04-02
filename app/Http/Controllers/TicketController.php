5e63a201b4817f63d82c4473f16b7b620fc977b9
Jan Ruhfus <unknownditx@gmail.com>
2021-04-02 22:47:49 +0200

    Load data for ticket admin

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TicketDepartment;
+use App\Models\TicketPreset;
+use App\Models\TicketSetting;
+use App\Models\TicketStatus;
+use App\Models\TicketTag;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
@@ -14,11 +19,33 @@ class TicketController extends Controller
         return view('tickets.list-tickets');
     }
 
-    public function adminTicket() {
-        return view('tickets.admin-ticket');
-    }
-
     public function viewTicket($ticketId) {
         return view('tickets.detail-ticket');
+    }
+
+    public function adminTicket() {
+        $data = [
+            'statues' => TicketStatus::all(),
+            'tags' => TicketTag::all(),
+            'departments' => TicketDepartment::all(),
+            'presets' => TicketPreset::all()
+        ];
+
+        return view('tickets.admin-ticket', $data);
+    }
+
+    /**
+     * Get setting value
+     * @param $settingName String Name of the setting
+     * @return bool
+     */
+    private function getSetting($settingName): bool {
+        $setting = TicketSetting::where('setting_name', $settingName)->first();
+
+        if ($setting != null) {
+            return $setting->setting_value;
+        }
+
+        return false;
     }
 }
